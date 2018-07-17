0aaace9bd03b6dcc57f8ce270ba94e7c2a67b98f
shanchuan.wang <shanchuan.wang@chainedfinance.com>
2018-07-17 11:43:05 +0800

    feat<token> rename

--- tokens/worker.go
+++ tokens/worker.go
@@ -16,7 +16,7 @@ var (
 )
 
 func ExcuteTransaction(done <-chan struct{}) {
-	ticker := time.NewTicker(300 * time.Second)
+	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
 
 	for {
